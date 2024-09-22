0d1cba0e1b0d06b41f1554417f687f30dadb2e75
Hassaan100 <110764076+Hassaan100@users.noreply.github.com>
2024-09-22 17:12:46 +0500

    commit

--- src/app/page.js
+++ src/app/page.js
@@ -352,7 +352,7 @@ export default function Home() {
               src={"/Images/Chili.png"}
             ></Image>
             <div className="px-4">
-              <h3 className="text-sm font-normal">Chanise Cabbage</h3>
+              <h3 className="text-sm font-normal">Green Chili</h3>
               <div className="flex justify-between w-full">
                 <div className="flex flex-col gap-2">
                   <h1 className="text-base font-medium">$14.99</h1>
@@ -389,7 +389,7 @@ export default function Home() {
               src={"/Images/corn.png"}
             ></Image>
             <div className="px-4">
-              <h3 className="text-sm font-normal">Chanise Cabbage</h3>
+              <h3 className="text-sm font-normal">Corn</h3>
               <div className="flex justify-between w-full">
                 <div className="flex flex-col gap-2">
                   <h1 className="text-base font-medium">$14.99</h1>
@@ -423,7 +423,7 @@ export default function Home() {
               className=" w-full   	"
               width={246}
               height={246}
-              src={"/Images/Lettuce.png"}
+              src={"/Images/GreenLettuce.png"}
             ></Image>
             <div className="px-4">
               <h3 className="text-sm font-normal">Chanise Cabbage</h3>
@@ -466,14 +466,53 @@ export default function Home() {
           <div className=" group col-span-4 bg-white cursor-pointer  transition-shadow duration-150	ease-in-out hover:border-[#2C742F]  hover:shadow-md hover:shadow-[#2c742e6d] ">
             <div className="flex gap-5 justify-center items-center border-solid border-[1px] py-3 border-[#E6E6E6] ">
               <Image
-                className=" w-20 h-20 "
+                className=" w-26 h-20 "
                 width={100}
                 height={100}
-                src={"/Images/apple.png"}
+                src={"/Images/greenapple.png"}
               ></Image>
 
               <div className="flex flex-col gap-2 w-[50%] ">
-                <h3 className="text-sm font-normal">Chanise Cabbage</h3>
+                <h3 className="text-sm font-normal">Green Apple</h3>
+                <h1 className="text-base font-medium group-hover:hidden">
+                  $14.99
+                </h1>
+                <div className="flex gap-1 group-hover:hidden">
+                  <FontAwesomeIcon icon={faStar} className="text-[#FF8A00]" />
+                  <FontAwesomeIcon icon={faStar} className="text-[#FF8A00]" />
+                  <FontAwesomeIcon icon={faStar} className="text-[#FF8A00]" />
+                  <FontAwesomeIcon icon={faStar} className="text-[#FF8A00]" />
+                  <FontAwesomeIcon icon={faStar} className="text-[#CCCCCC]" />
+                </div>
+                <div className="hidden  gap-3 group-hover:flex  items-center">
+                 
+                  <FontAwesomeIcon
+                    icon={faBagShopping}
+                    className="rounded-full border-[1px] border-solid border-[#9a9292] py-3 px-[12px] text-[11px] bg-[#00B207]"
+                  />
+                  <FontAwesomeIcon
+                    icon={faEye}
+                    className="rounded-full border-[1px] border-solid border-[#9a9292] py-3 px-[12px] text-[11px]"
+                  />
+                  <FontAwesomeIcon
+                    icon={faHeart}
+                    className="rounded-full border-[1px] border-solid border-[#9a9292] py-3 px-[12px]  text-[11px]"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className=" group col-span-4 bg-white cursor-pointer  transition-shadow duration-150	ease-in-out hover:border-[#2C742F]  hover:shadow-md hover:shadow-[#2c742e6d] ">
+            <div className="flex gap-5 justify-center items-center border-solid border-[1px] py-3 border-[#E6E6E6] ">
+              <Image
+                className=" w-26 h-20 "
+                width={100}
+                height={100}
+                src={"/Images/Eggplant.png"}
+              ></Image>
+
+              <div className="flex flex-col gap-2 w-[50%] ">
+                <h3 className="text-sm font-normal">Indian Malta</h3>
                 <h1 className="text-base font-medium group-hover:hidden">
                   $14.99
                 </h1>
@@ -505,53 +544,14 @@ export default function Home() {
           <div className=" group col-span-4 bg-white cursor-pointer  transition-shadow duration-150	ease-in-out hover:border-[#2C742F]  hover:shadow-md hover:shadow-[#2c742e6d] ">
             <div className="flex gap-5 justify-center items-center border-solid border-[1px] py-3 border-[#E6E6E6] ">
               <Image
-                className=" w-20 h-20 "
+                className=" w-26 h-20 "
                 width={100}
                 height={100}
-                src={"/Images/apple.png"}
+                src={"/Images/GreenLettuce.png"}
               ></Image>
 
               <div className="flex flex-col gap-2 w-[50%] ">
-                <h3 className="text-sm font-normal">Chanise Cabbage</h3>
-                <h1 className="text-base font-medium group-hover:hidden">
-                  $14.99
-                </h1>
-                <div className="flex gap-1 group-hover:hidden">
-                  <FontAwesomeIcon icon={faStar} className="text-[#FF8A00]" />
-                  <FontAwesomeIcon icon={faStar} className="text-[#FF8A00]" />
-                  <FontAwesomeIcon icon={faStar} className="text-[#FF8A00]" />
-                  <FontAwesomeIcon icon={faStar} className="text-[#FF8A00]" />
-                  <FontAwesomeIcon icon={faStar} className="text-[#CCCCCC]" />
-                </div>
-                <div className="hidden  gap-3 group-hover:flex  items-center">
-                  {" "}
-                  <FontAwesomeIcon
-                    icon={faBagShopping}
-                    className="rounded-full border-[1px] border-solid border-[#9a9292] py-3 px-[12px] text-[11px] bg-[#00B207]"
-                  />
-                  <FontAwesomeIcon
-                    icon={faEye}
-                    className="rounded-full border-[1px] border-solid border-[#9a9292] py-3 px-[12px] text-[11px]"
-                  />
-                  <FontAwesomeIcon
-                    icon={faHeart}
-                    className="rounded-full border-[1px] border-solid border-[#9a9292] py-3 px-[12px]  text-[11px]"
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className=" group col-span-4 bg-white cursor-pointer  transition-shadow duration-150	ease-in-out hover:border-[#2C742F]  hover:shadow-md hover:shadow-[#2c742e6d] ">
-            <div className="flex gap-5 justify-center items-center border-solid border-[1px] py-3 border-[#E6E6E6] ">
-              <Image
-                className=" w-20 h-20 "
-                width={100}
-                height={100}
-                src={"/Images/apple.png"}
-              ></Image>
-
-              <div className="flex flex-col gap-2 w-[50%] ">
-                <h3 className="text-sm font-normal">Chanise Cabbage</h3>
+                <h3 className="text-sm font-normal">Green Lettuce</h3>
                 <h1 className="text-base font-medium group-hover:hidden">
                   $14.99
                 </h1>
@@ -582,18 +582,19 @@ export default function Home() {
           </div>
         </div>
         <div className="w-full flex flex-col gap-5">
-          <h1 className="font-medium text-lg">Best Seller</h1>
+          <h1 className="font-medium text-lg">Best Seller
+          </h1>
           <div className=" group col-span-4 bg-white cursor-pointer  transition-shadow duration-150	ease-in-out hover:border-[#2C742F]  hover:shadow-md hover:shadow-[#2c742e6d] ">
             <div className="flex gap-5 justify-center items-center border-solid border-[1px] py-3 border-[#E6E6E6] ">
               <Image
-                className=" w-20 h-20 "
+                className=" w-26 h-20 "
                 width={100}
                 height={100}
-                src={"/Images/apple.png"}
+                src={"/Images/Eggplant.png"}
               ></Image>
 
               <div className="flex flex-col gap-2 w-[50%] ">
-                <h3 className="text-sm font-normal">Chanise Cabbage</h3>
+                <h3 className="text-sm font-normal">Eggplant</h3>
                 <h1 className="text-base font-medium group-hover:hidden">
                   $14.99
                 </h1>
@@ -625,14 +626,14 @@ export default function Home() {
           <div className=" group col-span-4 bg-white cursor-pointer  transition-shadow duration-150	ease-in-out hover:border-[#2C742F]  hover:shadow-md hover:shadow-[#2c742e6d] ">
             <div className="flex gap-5 justify-center items-center border-solid border-[1px] py-3 border-[#E6E6E6] ">
               <Image
-                className=" w-20 h-20 "
+                className=" w-26 h-20 "
                 width={100}
                 height={100}
-                src={"/Images/apple.png"}
+                src={"/Images/Capsicum.png"}
               ></Image>
 
               <div className="flex flex-col gap-2 w-[50%] ">
-                <h3 className="text-sm font-normal">Chanise Cabbage</h3>
+                <h3 className="text-sm font-normal">Red Capsicum</h3>
                 <h1 className="text-base font-medium group-hover:hidden">
                   $14.99
                 </h1>
@@ -664,14 +665,14 @@ export default function Home() {
           <div className=" group col-span-4 bg-white cursor-pointer  transition-shadow duration-150	ease-in-out hover:border-[#2C742F]  hover:shadow-md hover:shadow-[#2c742e6d] ">
             <div className="flex gap-5 justify-center items-center border-solid border-[1px] py-3 border-[#E6E6E6] ">
               <Image
-                className=" w-20 h-20 "
+                className=" w-26 h-20 "
                 width={100}
                 height={100}
-                src={"/Images/apple.png"}
+                src={"/Images/Tomatos.png"}
               ></Image>
 
               <div className="flex flex-col gap-2 w-[50%] ">
-                <h3 className="text-sm font-normal">Chanise Cabbage</h3>
+                <h3 className="text-sm font-normal">Red Tomatos</h3>
                 <h1 className="text-base font-medium group-hover:hidden">
                   $14.99
                 </h1>
@@ -706,14 +707,14 @@ export default function Home() {
           <div className=" group col-span-4 bg-white cursor-pointer  transition-shadow duration-150	ease-in-out hover:border-[#2C742F]  hover:shadow-md hover:shadow-[#2c742e6d] ">
             <div className="flex gap-5 justify-center items-center border-solid border-[1px] py-3 border-[#E6E6E6] ">
               <Image
-                className=" w-20 h-20 "
+                className=" w-26 h-20 "
                 width={100}
                 height={100}
-                src={"/Images/apple.png"}
+                src={"/Images/Potatos.png"}
               ></Image>
 
               <div className="flex flex-col gap-2 w-[50%] ">
-                <h3 className="text-sm font-normal">Chanise Cabbage</h3>
+                <h3 className="text-sm font-normal">Big Potatos</h3>
                 <h1 className="text-base font-medium group-hover:hidden">
                   $14.99
                 </h1>
@@ -745,14 +746,14 @@ export default function Home() {
           <div className=" group col-span-4 bg-white cursor-pointer  transition-shadow duration-150	ease-in-out hover:border-[#2C742F]  hover:shadow-md hover:shadow-[#2c742e6d] ">
             <div className="flex gap-5 justify-center items-center border-solid border-[1px] py-3 border-[#E6E6E6] ">
               <Image
-                className=" w-20 h-20 "
+                className=" w-26 h-20 "
                 width={100}
                 height={100}
-                src={"/Images/apple.png"}
+                src={"/Images/corn.png"}
               ></Image>
 
               <div className="flex flex-col gap-2 w-[50%] ">
-                <h3 className="text-sm font-normal">Chanise Cabbage</h3>
+                <h3 className="text-sm font-normal">Corn</h3>
                 <h1 className="text-base font-medium group-hover:hidden">
                   $14.99
                 </h1>
@@ -784,14 +785,14 @@ export default function Home() {
           <div className=" group col-span-4 bg-white cursor-pointer  transition-shadow duration-150	ease-in-out hover:border-[#2C742F]  hover:shadow-md hover:shadow-[#2c742e6d] ">
             <div className="flex gap-5 justify-center items-center border-solid border-[1px] py-3 border-[#E6E6E6] ">
               <Image
-                className=" w-20 h-20 "
+                className=" w-26 h-20 "
                 width={100}
                 height={100}
-                src={"/Images/apple.png"}
+                src={"/Images/cauliflower.png"}
               ></Image>
 
               <div className="flex flex-col gap-2 w-[50%] ">
-                <h3 className="text-sm font-normal">Chanise Cabbage</h3>
+                <h3 className="text-sm font-normal">Fresh cauliflower</h3>
                 <h1 className="text-base font-medium group-hover:hidden">
                   $14.99
                 </h1>
@@ -821,6 +822,7 @@ export default function Home() {
             </div>
           </div>
         </div>
+        
         <div className="   w-full flex justify-start items-center flex-col gap-5  pb-36 py-10  px-10 pt-10  bg-[url('/Images/bg3.png')] bg-cover	bg-bottom	">
           <p className="font-medium text-sm  text-black">HOt Sale</p>
           <h1 className="font-light text-[32px] text-black text-center">
