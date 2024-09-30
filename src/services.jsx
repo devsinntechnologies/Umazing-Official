5c5f71c5d52c3141e162d361daaa1da4e0c950ff
Hassaan100 <110764076+Hassaan100@users.noreply.github.com>
2024-10-01 00:14:26 +0500

    Changes

--- src/services.jsx
+++ src/services.jsx
@@ -1,1 +1,13 @@
+import axios from "axios";
+
 export const ServiceAPI = "http://97.74.89.204:4000";
+
+export const fetchCategories = async () => {
+  try {
+    const response = await axios.get(`${ServiceAPI}/category/getAllCategories`);
+    return response.data.data;
+  } catch (error) {
+    console.error("Error fetching categories:", error);
+    throw error;
+  }
+};
