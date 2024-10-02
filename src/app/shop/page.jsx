cbe822ca8aaf39b64c77602967b00e7567aa64fc
emanali262770 <emanali2838@gmail.com>
2024-10-02 06:34:41 +0500

    changes

--- src/app/shop/page.jsx
+++ src/app/shop/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { useSearchParams } from "next/navigation";
+
 import axios from "axios";
 import { useSearchParams } from "next/navigation"; // Import useSearchParams
 import { ShopAPI } from "@/Services"; // Import the fetchAPI function
