af25d44111b71fced4e46e3c0b1066b2c846e706
Muhammad Arslan <114974765+marslanmustafa@users.noreply.github.com>
2024-10-13 15:59:17 +0500

    done

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-    domains: ['97.74.89.204'],
-    },
-  };
-  
-  export default nextConfig;
-  
+const nextConfig = {};
+
+export default nextConfig;
--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,8 @@
-// Home.js
-"use client";
-
-import HeaderCategory from "@/components/Header/HeaderCategory";
-import { Banner } from "@/components/Home/Banner";
-import { ProductSection } from "@/components/Home/ProductSection";
-import DealSection from "@/components/Home/DealSection";
-import { BannerSection } from "@/components/Home/BannerSection";
-import { Team } from "@/components/Home/Team";
-import { Companylogos } from "@/components/Home/Companylogos";
-import Categories from "@/components/Home/Categories";
+import Image from "next/image";
+import CategoriesPage from "./components/Categories";
 
 export default function Home() {
   return (
-    <h1>Home</h1>
+    <CategoriesPage/>
   );
 }
