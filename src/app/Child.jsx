788437fc2619cb93470b9ee3868f4880e10c4340
Muhammad Arslan <114974765+marslanmustafa@users.noreply.github.com>
2024-10-21 03:33:42 +0500

    little changes

--- src/app/Child.jsx
+++ src/app/Child.jsx
@@ -3,29 +3,16 @@ import { useSelector } from "react-redux";
 import { usePathname } from "next/navigation";
 import Navbar from "@/components/Navbar/Navbar";
 import Footer from "@/components/Footer/Footer";
-import Link from "next/link";
-import { Plus } from "lucide-react";
-import {
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query';
-
-const queryClient = new QueryClient();
 
 export default function Child({ children }) {
-  const isLoggedIn = useSelector((state) => state.authSlice.isLoggedIn);
-  const hidePlusButtonPaths = ['/seller', '/seller/addProduct', '/seller/products'];
-  const pathname = usePathname();
 
   return (
     <>
-    <QueryClientProvider client={queryClient}>
       <Navbar />
       <div className="px-2 sm:px-4 md:px-6 lg:px-8 w-full">
       {children}
       <Footer />
       </div> 
-      </QueryClientProvider>
     </>
   );
 }
