e0fe061a14d1e000ebcd88d9ffc06baca5353ad3
Azan-Asim <148682737+Azan-Asim@users.noreply.github.com>
2024-11-14 18:25:13 +0500

    wishlist responsive issues resolved

--- src/app/wishlist/page.tsx
+++ src/app/wishlist/page.tsx
@@ -182,16 +182,18 @@ const Page: React.FC = () => {
 
   return (
     <>
-      <BreadCrumb />
-      <div className="w-full py-6">
-        <h1 className="text-lg md:text-xl lg:text-2xl font-semibold mb-6 text-center">
-          My Wishlist
-        </h1>
-        <div className="shadow-sm shadow-primary text-xs border-b border-gray-200 rounded-lg md:w-[85%] w-[95%] mx-auto">
+    <BreadCrumb />
+    <div className="w-full py-6">
+      <h1 className="text-lg md:text-xl lg:text-2xl font-semibold mb-6 text-center">
+        My Wishlist
+      </h1>
+      <div className="shadow-sm shadow-primary text-xs border-b border-gray-200 rounded-lg md:w-[85%] w-[95%] mx-auto overflow-x-auto">
+        <div className="min-w-[600px]">
           <DataTable columns={columns} data={wishlist} isLoading={isLoading} />
         </div>
       </div>
-    </> 
+    </div>
+  </>
   );
 };
 
