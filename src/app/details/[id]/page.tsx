e3809fcf6ea0b55e8e75995be515b96675f900f0
Azan-Asim <148682737+Azan-Asim@users.noreply.github.com>
2024-12-15 11:08:16 +0500

    ...

--- src/app/details/[id]/page.tsx
+++ src/app/details/[id]/page.tsx
@@ -200,7 +200,7 @@ const Page = () => {
                 </div>
                 <div className="flex items-center gap-3">
                   <p className="text-[#2C742F] text-[24px] font-medium">
-                    {productLoading ? <Skeleton className="h-6 w-16" /> : `{CURRENCY} ${product?.basePrice}`}
+                    {productLoading ? <Skeleton className="h-6 w-16" /> : `${CURRENCY} ${product?.basePrice}`}
                   </p>
                   <div className="w-[75px] h-[27px] rounded-[30px] text-destructive text-[14px] py-[3px] px-[10px]">
                     {productLoading ? <Skeleton className="h-4 w-10" /> : `${product?.baseQuantity} Qty`}
